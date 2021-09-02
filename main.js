ba61a56c78d87a95644c6a5a29fc38a867d2a0e3
Abdul Rehman <mehars.6925@gmail.com>
2021-09-02 13:00:53 +0500

    updated

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const bytenode = require('bytenode');
+const isPackaged = require('electron-is-packaged').isPackaged;
 const fs = require('fs');
 const v8 = require('v8');
 
 v8.setFlagsFromString('--no-lazy');
+
+if (!isPackaged) {
+  fs.unlinkSync(__dirname + '/app.jsc')
+}
 
 if (!fs.existsSync(__dirname + '/app.jsc')) {
   bytenode.compileFile(__dirname + '/app.js', __dirname + '/app.jsc');
