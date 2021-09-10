f7b1591bf82aca8d926416a2db88254e38c80034
Abdul Rehman <mehars.6925@gmail.com>
2021-09-10 12:52:12 +0500

    added reloader for electron

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const bytenode = require('bytenode');
 const isPackaged = require('electron-is-packaged').isPackaged;
 const fs = require('fs');
 const v8 = require('v8');
+const path = require('path');
 
 v8.setFlagsFromString('--no-lazy');
 
@@ -11,6 +12,13 @@ if (!isPackaged && fs.existsSync(__dirname + '/app.jsc')) {
 
 if (!fs.existsSync(__dirname + '/app.jsc')) {
   bytenode.compileFile(__dirname + '/app.js', __dirname + '/app.jsc');
+}
+
+
+if (!isPackaged) {
+  try {
+    require('electron-reload')(__dirname, { ignored: ['**.jsc'] });
+  } catch (_) { console.log('Error'); } 
 }
 
 require(__dirname + '/app.jsc');
