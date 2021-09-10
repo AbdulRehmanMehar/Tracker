ecf4fc6c600e4f9440a68be045ddae94f350ebcd
Abdul Rehman <mehars.6925@gmail.com>
2021-09-10 12:53:38 +0500

    bundles react app

---
+++ webpack.mix.js
@@ -0,0 +1,35 @@
+const mix = require('laravel-mix')
+const nodeExternals = require('webpack-node-externals');
+
+mix.webpackConfig({
+  target: 'node',
+  externals: [ nodeExternals() ],
+  node: {
+      __dirname: false,
+      __filename: false
+  }
+})
+
+mix.webpackConfig({
+    resolve: {
+        fallback: {
+            "fs": false,
+            "tls": false,
+            "net": false,
+            "path": false,
+            "zlib": false,
+            "http": false,
+            "https": false,
+            "stream": false,
+            "crypto": false,
+            "os": false,
+            "child_process": false,
+            "iconv": false,
+            "crypto-browserify": require.resolve('crypto-browserify')
+        },
+        
+    }
+})
+  .setPublicPath('./bundle')
+  .js('react/src/index.js', 'bundle/js').react()
+  .setResourceRoot('./bundle/');
