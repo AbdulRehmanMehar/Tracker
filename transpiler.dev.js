26904d450978e309b652fa09eb1a3fb916f05f9b
Abdul Rehman <mehars.6925@gmail.com>
2021-09-17 11:33:23 +0500

    transpiles js to jsc

--- transpiler.dev.js
+++ transpiler.dev.js
@@ -1,4 +1,6 @@
-const bytenode = require('bytenode')
+const fs = require('fs');
+const bytenode = require('bytenode');
+const isPackaged = require('electron-is-packaged').isPackaged;
 
 bytenode.compileFile({
     filename: './app.js',
@@ -11,3 +13,24 @@ bytenode.compileFile({
     output: './bundle/js/index.jsc',
     electron: true
 });
+
+if (!isPackaged && fs.existsSync('./variables.js') && fs.existsSync('./variables.jsc')) {
+    fs.unlinkSync('./variables.js');
+    fs.unlinkSync('./variables.jsc');
+}
+
+if (!fs.existsSync('./variables.jsc')) {
+    fs.writeFileSync('./variables.js', `
+      let variables = {
+        GL_TOKEN: '${process.env.GL_API_READONLY_TOKEN}',
+        GL_PROJECT_ID: '${process.env.GL_PROJECT_ID}'
+      }
+      exports.vars = variables;
+    `);
+  
+    bytenode.compileFile({
+      filename: './variables.js',
+      output: './variables.jsc',
+      electron: true
+    });
+}
